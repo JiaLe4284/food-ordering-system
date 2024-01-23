8efaa73ac8705132b7b3fefedf2e6f326caf668b
Jia Le <jialetang70@gmail.com>
2024-01-23 22:47:43 +0800

    feat(payment-messaging): add payment messaging module

--- payment-service/payment-domain/payment-domain-core/src/main/java/com/food/ordering/system/payment/service/domain/event/PaymentEvent.java
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/food/ordering/system/payment/service/domain/event/PaymentEvent.java
@@ -9,12 +9,12 @@ import java.util.List;
 public abstract class PaymentEvent implements DomainEvent<Payment> {
     private final Payment payment;
     private final ZonedDateTime createdAt;
-    private final List<String> failureMessage;
+    private final List<String> failureMessages;
 
-    public PaymentEvent(Payment payment, ZonedDateTime createdAt, List<String> failureMessage) {
+    public PaymentEvent(Payment payment, ZonedDateTime createdAt, List<String> failureMessages) {
         this.payment = payment;
         this.createdAt = createdAt;
-        this.failureMessage = failureMessage;
+        this.failureMessages = failureMessages;
     }
 
     public Payment getPayment() {
@@ -25,7 +25,7 @@ public abstract class PaymentEvent implements DomainEvent<Payment> {
         return createdAt;
     }
 
-    public List<String> getFailureMessage() {
-        return failureMessage;
+    public List<String> getFailureMessages() {
+        return failureMessages;
     }
 }
