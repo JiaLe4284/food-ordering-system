77089b3cecb01738da435131fc665278d73502f9
Jia Le <jialetang70@gmail.com>
2024-01-27 22:18:24 +0800

    fix: wrong kafka consumer header

--- infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/KafkaMessageHelper.java
+++ infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/KafkaMessageHelper.java
@@ -1,8 +1,5 @@
 package com.food.ordering.system.kafka.producer;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.food.ordering.system.order.service.domain.exception.OrderDomainException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.kafka.support.SendResult;
@@ -13,21 +10,6 @@ import java.util.function.BiConsumer;
 @Slf4j
 @Component
 public class KafkaMessageHelper {
-
-    private final ObjectMapper objectMapper;
-
-    public KafkaMessageHelper(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
-
-    public <T> T getOrderEventPayload(String payload, Class<T> outputType) {
-        try {
-            return objectMapper.readValue(payload, outputType);
-        } catch (JsonProcessingException e) {
-            log.error("Could not read {} object!", outputType.getName(), e);
-            throw new OrderDomainException("Could not read " + outputType.getName() + " object!", e);
-        }
-    }
 
     public <T> BiConsumer<SendResult<String, T>, Throwable> getKafkaCallback(String responseTopicName,
                                                                              T avroModel, String orderId,
--- payment-service/payment-messaging/src/main/java/com/food/ordering/system/payment/service/messaging/listener/kafka/PaymentRequestKafkaListener.java
+++ payment-service/payment-messaging/src/main/java/com/food/ordering/system/payment/service/messaging/listener/kafka/PaymentRequestKafkaListener.java
@@ -32,8 +32,8 @@ public class PaymentRequestKafkaListener implements KafkaConsumer<PaymentRequest
     @KafkaListener(id = "${kafka-consumer-config.payment-consumer-group-id}",
             topics = "${payment-service.payment-request-topic-name}")
     public void receive(@Payload List<PaymentRequestAvroModel> message,
-                        @Header(KafkaHeaders.KEY) List<String> keys,
-                        @Header(KafkaHeaders.PARTITION) List<Integer> partitions,
+                        @Header(KafkaHeaders.RECEIVED_KEY) List<String> keys,
+                        @Header(KafkaHeaders.RECEIVED_PARTITION) List<Integer> partitions,
                         @Header(KafkaHeaders.OFFSET) List<Long> offsets) {
         log.info("{} number of payment requests received with keys:{}, partitions:{} and offsets: {}",
                 message.size(),
