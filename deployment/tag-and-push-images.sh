474038c8b97fbec4fedb70b5b5c3cef9a1063b5c
Jia Le <jialetang70@gmail.com>
2024-02-16 23:30:35 +0800

    feat(deployment): update artifactory registry and project id

--- deployment/tag-and-push-images.sh
+++ deployment/tag-and-push-images.sh
@@ -1,11 +1,11 @@
 gcloud auth login
 
-docker tag com.food.ordering.system/order.service:$1 europe-west4-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/order.service:$1
-docker tag com.food.ordering.system/payment.service:$1 europe-west4-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/payment.service:$1
-docker tag com.food.ordering.system/restaurant.service:$1 europe-west4-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/restaurant.service:$1
-docker tag com.food.ordering.system/customer.service:$1 europe-west4-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/customer.service:$1
+docker tag com.food.ordering.system/order.service:1.0-SNAPSHOT asia-southeast1-docker.pkg.dev/peppy-caster-414418/food-ordering-system-repo/order.service:$1
+docker tag com.food.ordering.system/payment.service:1.0-SNAPSHOT asia-southeast1-docker.pkg.dev/peppy-caster-414418/food-ordering-system-repo/payment.service:$1
+docker tag com.food.ordering.system/restaurant.service:1.0-SNAPSHOT asia-southeast1-docker.pkg.dev/peppy-caster-414418/food-ordering-system-repo/restaurant.service:$1
+docker tag com.food.ordering.system/customer.service:1.0-SNAPSHOT asia-southeast1-docker.pkg.dev/peppy-caster-414418/food-ordering-system-repo/customer.service:$1
 
-docker push europe-west4-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/order.service:$1
-docker push europe-west4-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/payment.service:$1
-docker push europe-west4-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/restaurant.service:$1
-docker push europe-west4-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/customer.service:$1
+docker push asia-southeast1-docker.pkg.dev/peppy-caster-414418/food-ordering-system-repo/order.service:$1
+docker push asia-southeast1-docker.pkg.dev/peppy-caster-414418/food-ordering-system-repo/payment.service:$1
+docker push asia-southeast1-docker.pkg.dev/peppy-caster-414418/food-ordering-system-repo/restaurant.service:$1
+docker push asia-southeast1-docker.pkg.dev/peppy-caster-414418/food-ordering-system-repo/customer.service:$1
